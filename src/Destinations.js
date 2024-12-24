fd8bb295ed90228749aa9e7cd7ca634eff5828e0
ToluwalaseO <benjamoseni@gmail.com>
2024-12-24 02:46:13 -0800

    almost done2

--- src/Destinations.js
+++ src/Destinations.js
@@ -4,11 +4,11 @@ import "./Destinations.css";
 const Destinations = () => {
   return (
     <section className="destinations">
-      <h2>Different destinations</h2>
+      <h1>Different destinations</h1>
       <div className="destination-content">
         <p1>We explore the diverse ways in which this</p1>
         <p2>technology can revolutionize work across</p2>
-        <p3>across various industries and fields.</p3>
+        <p3>various industries and fields.</p3>
         <p4>Discover how AI can streamline processes</p4>
         <p5>and elevate your work to new heights.</p5>
         <a href="#applications">Discover all applications →</a>
